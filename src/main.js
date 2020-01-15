a6d57decf6f1d38ae45d577dfd870c39317c0d5f
李顺世 <525311971@qq.com>
2020-01-15 17:06:01 +0800

    2020 1/15 admin vue+elmentui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import './style.css'
+import http from './http'
+Vue.prototype.$http = http
 Vue.config.productionTip = false
 
 new Vue({
